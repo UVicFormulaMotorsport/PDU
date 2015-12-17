8c0c7fc378b60e46b663507b171ced978492c4d8
Darryl Ring <darryl@darrylring.com>
2015-12-17 14:07:04 -0800

    firmware: Initial firmware skeleton

---
+++ firmware/src/pdu.c
@@ -0,0 +1,34 @@
+/*
+  This file is part of the UVic Formula Motorsports PDU project.
+
+  Copyright (c) 2015 UVic Formula Motorsports
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "pdu.h"
+
+
+int
+main(void)
+{
+	pdu_adc_init();
+	pdu_can_init();
+	
+	for (;;) {
+
+	}
+
+	return 0;
+}
