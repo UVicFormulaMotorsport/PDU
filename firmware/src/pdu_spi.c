07e8c1d5ed9f0812462c8c890177c23fdc56f6d3
sdburt <seandburt@gmail.com>
2017-03-13 17:14:19 -0700

    switch SPI to slave and add MISO SPI

--- firmware/src/pdu_spi.c
+++ firmware/src/pdu_spi.c
@@ -4,26 +4,38 @@
 
 
 // DATA Direction Register controlling pins
-DDR_SPI = DDA1; // example
+DDR_SPI_1 = DDRB;
+
+// DATA Direction Register controlling pins
+DDR_SPI_2 = DDRA;
 
 // Data Direction bits for MOSI 
-DD_MOSI = DDB2; // example
+DD_MOSI = DDA3;
+
+// Data Direction bits for MISO 
+DD_MISO = DDB3;
 
 // Data Direction bits for SCK 
 DD_SCK = DDRB; // example
 
 void SPI_init() {
-    // SET MOSI and SCK output, all others input
     
-    DDR_SPI = (1 << DD_MOSI) | (1 << DD_SCK);
+    // SET MOSI output, all others input
+    DDR_SPI_1 = (1 << DD_MOSI);
 
-    // enable SPI, set to master. Add clock rate????
-    SPCR = (1 << SPE) | (1 << MSTR); 
+    // SET MISO output, all others input
+    DDR_SPI_1 = (1 << DD_MISO));
+
+    // enable SPI.
+    SPCR = (1 << SPE); 
 }
 
-void SPI_transmit() {
-    // start transmittion?
-    SPDR = cData;
+char SPI_Receive() {
+    
+    // wait for reception complete
 
     while(! SPSR & (1 << SPIF));
+
+    // return data register
+    return SPDR
 }
